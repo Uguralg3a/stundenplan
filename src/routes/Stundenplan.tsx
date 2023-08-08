c438ca28059785f84b6a794993041fa5123a48aa
Ugur <58608648+Uguralg3a@users.noreply.github.com>
2023-08-08 20:56:26 +0200

    Update Stundenplan.tsx

--- src/routes/Stundenplan.tsx
+++ src/routes/Stundenplan.tsx
@@ -1,7 +1,7 @@
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
 
-export default function Home() {
+export default function Stundenplan() {
   return (
     <main>
 <table class="tg">
