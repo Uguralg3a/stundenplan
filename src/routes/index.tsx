e6d52e2bc33783b0810768e726aabcb86e17c602
Ugur <58608648+Uguralg3a@users.noreply.github.com>
2023-08-08 21:24:06 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,6 @@ import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
-    <style>
-      body {
-        background-color: black;
-      }
-      h1{
-        text-align: center;
-      font-size: 50%;
-      }
-    </style>
     <main>
     <h1>Ain't nothin here</h1>
     </main>
