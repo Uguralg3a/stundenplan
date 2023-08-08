714bd3f6db564f85377794b6bc76a63e65580680
Ugur <58608648+Uguralg3a@users.noreply.github.com>
2023-08-08 20:52:36 +0200

    Create index.tsx

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import { Title } from "solid-start";
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <style>
+      body {
+        background-color: black;
+      }
+      h1{
+        text-align: center;
+      font-size: 50%;
+      }
+    </style>
+    <main>
+    <h1>Ain't nothin here</h1>
+    </main>
+  );
+      }
