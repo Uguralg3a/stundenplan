b7ba035672d044eefff42a9f263abaf8f15992ba
Ugur <58608648+Uguralg3a@users.noreply.github.com>
2023-08-08 21:31:45 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,22 @@
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
+import React from "react";
 
 export default function Home() {
   return (
-    <main>
-       <style>
-      body {
-        background-color: black;
-      }
-      h1{
-        text-align: center;
-      font-size: 50%;
+    const bodys:React.CSSProperties ={
+       background: black;
+    }
+
+const h1s:React.CSSProperties = {
+  text-align: center;
+  font-size: 50%;
          color: white;
-      }
-    </style>
-    <h1>Ain't nothin here</h1>
+}
+    <main style={bodys}>
+       
+      
+    <h1 style={h1s}>Ain't nothin here</h1>
     </main>
   );
       }
