bb68c6d4003aa8f3eed2a8449d2d3ff61a4ba9b2
Ugur <58608648+Uguralg3a@users.noreply.github.com>
2023-08-08 14:40:45 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,89 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+<table class="tg">
+  <tr>
+    <th class="tg-031e"></th>
+    <th class="tg-031e">Montag</th>
+    <th class="tg-031e">Dienstag</th>
+    <th class="tg-031e">Mittwoch</th>
+    <th class="tg-031e">Donnerstag</th>
+    <th class="tg-031e">Freitag</th>
+  </tr>
+  <tr>
+    <td class="tg-031e">08:00 Uhr</td>
+    <td class="tg-djqx">Englisch</td>
+    <td class="tg-5fb6">Deutsch</td>
+    <td class="tg-4q7z">Mathe</td>
+    <td class="tg-031e"></td>
+    <td class="tg-82n6">Geschichte</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">08:45 Uhr</td>
+    <td class="tg-djqx">Englisch</td>
+    <td class="tg-5fb6">Deutsch</td>
+    <td class="tg-4q7z">Mathe</td>
+    <td class="tg-031e"></td>
+    <td class="tg-82n6">Geschichte</td>
+  </tr>
+  <tr>
+    <td class="tg-031e" colspan="6">09:30 Uhr | Pause</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">09:55 Uhr</td>
+    <td class="tg-b4hr">Physik</td>
+    <td class="tg-4dp2">Politik</td>
+    <td class="tg-031e">Sport</td>
+    <td class="tg-8o5d">Erdkunde&nbsp;</td>
+    <td class="tg-5fb6">Deutsch</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">10:40 Uhr</td>
+    <td class="tg-b4hr">Physik</td>
+    <td class="tg-4dp2">Politik</td>
+    <td class="tg-cxkv">Sport</td>
+    <td class="tg-8o5d">Erdkunde</td>
+    <td class="tg-5fb6">Deutsch</td>
+  </tr>
+  <tr>
+    <td class="tg-031e" colspan="6">11:25 Uhr | Pause</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">11:50 Uhr</td>
+    <td class="tg-031e">Chemie</td>
+    <td class="tg-031e">Kurs</td>
+    <td class="tg-8e65">Englisch</td>
+    <td class="tg-031e">Kurs</td>
+    <td class="tg-4q7z">Mathe</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">12:35 Uhr</td>
+    <td class="tg-031e">Chemie</td>
+    <td class="tg-031e">Kurs</td>
+    <td class="tg-p2e8">Englisch</td>
+    <td class="tg-031e">ZUG</td>
+    <td class="tg-4q7z">Mathe</td>
+  </tr>
+  <tr>
+    <td class="tg-031e" colspan="6">13:20 Uhr | Mittagspause / Schulschluss</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">14:15 Uhr</td>
+    <td class="tg-031e">Religion/Philosophie</td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+  </tr>
+  <tr>
+    <td class="tg-031e">15:00 Uhr</td>
+    <td class="tg-031e">Religion/Philosophie</td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+  </tr>
+</table>
     </main>
   );
 }
