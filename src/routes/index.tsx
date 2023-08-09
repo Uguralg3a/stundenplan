b0210b90e2cc07f3be3cdbaabc1fabbd8de1334f
Ugur <58608648+Uguralg3a@users.noreply.github.com>
2023-08-09 07:11:05 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,8 @@
 import { Title } from "solid-start";
-import Counter from "~/components/Counter";
-import React from "react";
 
 export default function Home() {
   return (
- 
     <main>
-       
-      
     <h1>Ain't nothin here</h1>
     </main>
   );
