c00d0a37ea3ecbe24085a2eb5c1ab102e8891e64
Ugur <58608648+Uguralg3a@users.noreply.github.com>
2023-08-09 07:08:16 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,19 +4,11 @@ import React from "react";
 
 export default function Home() {
   return (
-    const bodys:React.CSSProperties ={
-       background: black;
-    }
-
-const h1s:React.CSSProperties = {
-  text-align: center;
-  font-size: 50%;
-         color: white;
-}
-    <main style={bodys}>
+ 
+    <main>
        
       
-    <h1 style={h1s}>Ain't nothin here</h1>
+    <h1>Ain't nothin here</h1>
     </main>
   );
       }
