af711bdf6fa2b79e9ba19c4443cc53c7d8a2f673
Ugur <58608648+Uguralg3a@users.noreply.github.com>
2023-09-14 20:45:33 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,8 @@ import { Title } from "solid-start";
 export default function Home() {
   return (
     <main>
-    <h1>Ain't nothin here</h1>
+    <h1>Willkommen auf der Seite der Klasse 8a</h1>
+    <h5 style="color:red">Hier gibts nicht zu sehen! Stundenplan gibt es <a href="https://ghr-8a.netlify.app/stundenplan">Hier</a></h5>
     </main>
   );
       }
